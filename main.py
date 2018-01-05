67d344dda56d37681aa59c9a706929be2639277c
Deodamnatus <jwgsub@gmail.com>
2018-01-05 11:10:34 -0800

    Updated

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def generateDecks(numDeck):
         suit = ''
         value = 0
 
-    for suit in ['Diamond', 'Heart', 'Spade', 'Clover']:
+    for suit in ['Diamond', 'Heart', 'Spade', 'Club']:
         for value in range(1, 14):
             currCard = Card()
             currCard.suit = suit
@@ -22,6 +22,50 @@ def generateDecks(numDeck):
     if numDeck == 1:
         return deck
     return deck + generateDecks(numDeck-1)
+
+def printCards(handlist):
+    print('\n' * 100)
+    print("You" end='\t\t')
+    for player in range(2,len(handlist) + 1):
+        print("Player ", player, end ='\t\t')
+    print("Dealer")
+    maxCardsToPrint= 0
+    for player in range(0,len(handlist+1)):
+        if len(handlist[player])>maxCardsToPrint:
+            maxCardsToPrint = len(handlist[player])
+
+    for card in range(0,maxCardsToPrint))
+        for player in range(0,len(handlist)+1):
+            if player == 0 and card >= 1:
+                print("- -", end='\t\t')
+            else:
+                try:
+                    currCard = handlist[player[card]]
+                    if currCard.card <= 10:
+                        print(currCard.card, end =' ')
+                    elif currCard.card == 11:
+                        print("J", end=' ')
+                    elif currCard.card == 12:
+                        print("Q", end=' ')
+                    elif currCard.card == 13:
+                        print("K", end=' ')
+                    spade = "♠"
+                    heart = "♥"
+                    diamond = "♦"
+                    club = "♣"
+                    if currCard.suit == "Spade":
+                        print(spade, end = '\t\t')
+                    elif currCard.suit == 'Heart':
+                        print(heart, end = '\t\t')
+                    elif currCard.suit == 'Diamond':
+                        print(diamond, end = '\t\t')
+                    elif currCard.suit == 'Club':
+                        print(club, end = '\t\t')
+
+                except IndexError:
+                    print('\t\t')
+
+
 
 # decide what to do as dealer
 def dealerChoice(dealerpoints):
@@ -37,8 +81,13 @@ def PlayGame():
     print("Welcome to Blackjack!")
 
     # new game
-    numPlayers = int(input('How many players (excluding dealer)? '))
-    numDecks = int(input('How many decks? '))
+    numPlayers = ''
+    while numPlayers not in [0,1,2,3]:
+        numPlayers = int(input('How many AI players (0-3)?'))
+
+    numDecks = 0
+    while numDecks not in [1,2,3,4,5,6]:
+        numDecks = int(input('How many decks (1-6)? '))
 
     #generate a deck with numDecks decks and shuffle it
     deck = generateDecks(numDecks)
@@ -52,8 +101,9 @@ def PlayGame():
         del deck[0:2]
 
     currplayer = 1
-    playerwon = false
-    while playerwon == false:
+    won = None
+    while won == None:
+
         hit = ''
         #if dealer's turn
         if currplayer == 0:
