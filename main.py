f04403db8b2a7b387089f5d9b009a7a66c50b270
Deodamnatus <jwgsub@gmail.com>
2018-01-06 12:39:34 -0800

    Updated

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 from random import shuffle
+import sys
+
+def printTitle():
+    print(
+    '''
+     ____  _            _     _            _    
+    | __ )| | __ _  ___| | __(_) __ _  ___| | __
+    |  _ \| |/ _` |/ __| |/ /| |/ _` |/ __| |/ /
+    | |_) | | (_| | (__|   < | | (_| | (__|   < 
+    |____/|_|\__,_|\___|_|\__/ |\__,_|\___|_|\_\
+                           |__/             
+    ''')
+
+
+def totalPoints(handlist, playernumber):
+    total = 0
+    for card in handlist[playernumber]:
+        total += card.value
+
+    return total
 
 # returns ordered deck
 def generateDecks(numDeck):
@@ -13,7 +33,9 @@ def generateDecks(numDeck):
             currCard = Card()
             currCard.suit = suit
             currCard.card = value
-            if value >=10:
+            if value == 1:
+                currCard.value = 11
+            elif value >=10:
                 currCard.value = 10
             else:
                 currCard.value = value
@@ -23,29 +45,34 @@ def generateDecks(numDeck):
         return deck
     return deck + generateDecks(numDeck-1)
 
+
 def printCards(handlist):
     print('\n' * 100)
-    print("You", end='\t\t')
-    for player in range(2,len(handlist) + 1):def printCards(handlist):
-    print('\n' * 100)
-    print("You" end='\t\t')
-    for player in range(2,len(handlist) + 1):
-        print("Player ", player, end ='\t\t')
-    print("Dealer")
-    maxCardsToPrint= 0
-    for player in range(0,len(handlist+1)):
-        if len(handlist[player])>maxCardsToPrint:
+    print("You     ", end='\t\t')
+
+    for player in range(2, len(handlist)):
+        print("Player ", player, end='\t\t')
+    print("Dealer  ")
+    maxCardsToPrint = 0
+    for player in range(0, len(handlist)):
+        if len(handlist[player]) > maxCardsToPrint:
             maxCardsToPrint = len(handlist[player])
 
-    for card in range(0,maxCardsToPrint))
-        for player in range(0,len(handlist)+1):
-            if player == 0 and card >= 1:
-                print("- -", end='\t\t')
+    for card in range(0, maxCardsToPrint):
+        for player in range(0, len(handlist)):
+            if player == len(handlist) - 1 and card >= 1:
+                print("- -", end='\n')
             else:
+                if player == len(handlist) - 1:
+                    endvar = '\n'
+                else:
+                    endvar = '     \t\t'
                 try:
                     currCard = handlist[player][card]
-                    if currCard.card <= 10:
-                        print(currCard.card, end =' ')
+                    if currCard.card == 1:
+                        print("A", end =' ')
+                    elif currCard.card <= 10:
+                        print(currCard.card, end=' ')
                     elif currCard.card == 11:
                         print("J", end=' ')
                     elif currCard.card == 12:
@@ -56,78 +83,61 @@ def printCards(handlist):
                     heart = "♥"
                     diamond = "♦"
                     club = "♣"
+
                     if currCard.suit == "Spade":
-                        print(spade, end = '\t\t')
+                        print(spade, end=endvar)
                     elif currCard.suit == 'Heart':
-                        print(heart, end = '\t\t')
+                        print(heart, end=endvar)
                     elif currCard.suit == 'Diamond':
-                        print(diamond, end = '\t\t')
+                        print(diamond, end=endvar)
                     elif currCard.suit == 'Club':
-                        print(club, end = '\t\t')
+                        print(club, end=endvar)
 
                 except IndexError:
-                    print('\t\t')
-        print("Player ", player, end ='\t\t')
-    print("Dealer")
-    maxCardsToPrint= 0
-    for player in range(0,len(handlist+1)):
-        if len(handlist[player])>maxCardsToPrint:
-            maxCardsToPrint = len(handlist[player])
+                    print('\t', end=endvar)
+    print('')
+    for player in range(0,len(handlist)):
+        print("___     ", end = '\t\t')
+    for player in range(0,len(handlist)):
+        print(totalPoints(handlist,player), '      ', end = '\t\t')
+    print("\n" * 10)
 
-    for card in range(0,maxCardsToPrint):
-        for player in range(0,len(handlist)+1):
-            if player == 0 and card >= 1:
-                print("- -", end='\t\t')
-            else:
-                try:
-                    currCard = handlist[player][card]
-                    if currCard.card <= 10:
-                        print(currCard.card, end =' ')
-                    elif currCard.card == 11:
-                        print("J", end=' ')
-                    elif currCard.card == 12:
-                        print("Q", end=' ')
-                    elif currCard.card == 13:
-                        print("K", end=' ')
-                    spade = "♠"
-                    heart = "♥"
-                    diamond = "♦"
-                    club = "♣"
-                    if currCard.suit == "Spade":
-                        print(spade, end = '\t\t')
-                    elif currCard.suit == 'Heart':
-                        print(heart, end = '\t\t')
-                    elif currCard.suit == 'Diamond':
-                        print(diamond, end = '\t\t')
-                    elif currCard.suit == 'Club':
-                        print(club, end = '\t\t')
+def getnextplayer(folded,currplayer):
+    if currplayer + 1 == len(folded):
+        if folded[0] == 0:
+            return 0
+        else:
+            getnextplayer(folded,0)
 
-                except IndexError:
-                    print('\t\t')
-
-
+    elif folded[currplayer + 1] == 0:
+        return currplayer+1
+    else:
+        return getnextplayer(folded, currplayer + 1)
 
 # decide what to do as dealer
-def dealerChoice(dealerpoints):
+def dealerChoice(handlist):
     #check for probability of drawing winning card if you want we just did basic
+    dealerpoints =0
+    for card in handlist[-1]:
+        dealerpoints += card.value
+
     if dealerpoints < 16:
         return True
     else:
         return False
 
 def PlayGame():
-    deck = []
-    #provide menu and get choice
-    print("Welcome to Blackjack!")
-
+    print("Welcome to")
+    printTitle()
+    print("\n"*5)
     # new game
     numPlayers = ''
     while numPlayers not in [0,1,2,3]:
-        numPlayers = int(input('How many AI players (0-3)?'))
+        numPlayers = int(input('How many AI players (0-3)?\n:'))
 
     numDecks = 0
     while numDecks not in [1,2,3,4,5,6]:
-        numDecks = int(input('How many decks (1-6)? '))
+        numDecks = int(input('How many decks (1-6)?\n:'))
 
     #generate a deck with numDecks decks and shuffle it
     deck = generateDecks(numDecks)
@@ -136,26 +146,68 @@ def PlayGame():
     # deal initial cards
     # handlist is in format of [ [player0 card0, player0 card0], ...]
     handlist = []
+    folded = {}
     for player in range(0,numPlayers+2):
         handlist.append([deck[0],deck[1]])
         del deck[0:2]
+        if totalPoints(handlist, player) == 21:
+            folded[player] = 1
+        else:
+            folded[player] = 0
 
-    currplayer = 1
-    won = None
-    while won == None:
+    currplayer = 0
+    while 0 in folded:
 
         hit = ''
         #if dealer's turn
-        if currplayer == 0:
-            totalpoints = 0
-            for card in handlist[0]:
-                totalpoints+=card.value
-            hit = dealerChoice(totalpoints)
+        if currplayer == numPlayers+1:
+            hit = dealerChoice(totalPoints(handlist,-1))
         #if players turn
-        elif currplayer == 1:
+        elif currplayer == 0:
             while hit not in [0,1]:
                 printCards(handlist)
-                hit = input("Hit or miss?")
+                hit = input("Hit or miss (0,1)?\n:")
+        else:
+            hit = dealerChoice(totalPoints(handlist,-1))
+            # hit = AI hit function
+        if hit == 1:
+            handlist[currplayer].append(deck[0])
+            del deck[0]
+            if totalPoints(handlist,currplayer) == 21:
+                folded[currplayer] = 1
+            elif totalPoints(handlist, currplayer) > 21:
+                for cardnumber in range(0,len(handlist[currplayer])):
+                    Card = handlist[currplayer][cardnumber]
+                    if Card.card == 1 and Card.value == 11:
+                        Card.value = 1
+                        break
+                if totalPoints(handlist, currplayer)>21:
+                    folded[currplayer] = 1
+        else:
+            folded[currplayer] = 1
+
+        if 0 in folded:
+            currplayer = getnextplayer(folded,currplayer)
+
+
+    printCards(handlist)
+    winner = 0
+    for player in range(0,numPlayers+2):
+        if totalPoints(handlist,player) == 21 and player == 0:
+            print("You won!!!")
+            winner = 1
+        if totalPoints(handlist,player) == 21 and player == numPlayers+ 1:
+            print("Dealer won!!!")
+            winner = 1
+        elif totalPoints(handlist, player) == 21:
+            print("Player ",player, "won!!!")
+            winner = 1
+    if winner == 0:
+        winner = ''
+        for player
+
+
+
 
 
 
